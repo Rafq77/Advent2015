5ffc404021aa89761d14e7baab6e04caeb4ad275
Rafq77 <sokol-77@wp.pl>
2016-12-07 20:48:06 +0100

    Advent2016: Added Day07 python solution (now python input is also read from file)

---
+++ Advent2016/Day/Day_07.py
@@ -0,0 +1,54 @@
+import os
+import string
+import re
+
+pBrckts = """\[[a-z]+\]"""
+from collections import Counter
+
+def Abba(x):
+    for i in range(len(x)-3):
+        if (x[i] == x[i+3] and x[i+1] == x[i+2] and x[i] != x[i+1]):
+            return True
+    return False
+
+def Kek(kek, eke):
+    for i in range(len(kek)-2):
+        if (kek[i] == kek[i+2] and kek[i] != kek[i+1]):
+            for j in range(len(eke)-2):
+                if (kek[i] == eke[j+1] and \
+                    kek[i+1] == eke[j] and \
+                    eke[j] == eke[j+2] and \
+                    eke[j] != eke[j+1]):
+                    return True
+    return False
+    
+fd = open("""../Resources/Day_07.txt""", 'r')
+s = fd.read()
+words = s.split('\n')
+cnt = 0
+topKek = 0
+
+for word in words:
+    fnds = re.findall(pBrckts, word)
+    add = True
+    if (list(filter(lambda x: Abba(x), fnds))):
+       add = False
+
+    for match in fnds:
+        word = word.replace(match, '|')
+
+    if (Kek(word, "".join(fnds))):
+        topKek += 1
+    if (Abba(word) and add):
+        cnt+=1
+
+print(cnt)
+print("Top kek " + str(topKek))
+    
+
+
+
+
+    
+
+    
